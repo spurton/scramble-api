b02c0d56eb63ce88806884873d766166fc88ba29
Ian Joseph Wilson <ianjosephwilson@gmail.com>
2013-06-30 17:12:35 -0700

    Add 404 exception helper.

--- scrambleapi/utils.py
+++ scrambleapi/utils.py
@@ -8,3 +8,11 @@ class _401(exc.HTTPError):
         Response.__init__(self, write_json(body))
         self.status = 401
         self.content_type = 'application/json'
+
+
+class _404(exc.HTTPError):
+    def __init__(self, msg='Not Found'):
+        body = {'status': 404, 'message': msg}
+        Response.__init__(self, write_json(body))
+        self.status = 404
+        self.content_type = 'application/json'
